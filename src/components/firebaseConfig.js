1fa29d3be5b8df797a824e0697f5cf6c3314aeec
nedab <nedab@uw.edu>
2024-06-03 04:02:04 -0700

    neda 1

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhwSRw3yzZih9Ri0tZcPjA2pfM1hY2Y8g",
+  authDomain: "medlog-68fbc.firebaseapp.com",
+  projectId: "medlog-68fbc",
+  storageBucket: "medlog-68fbc.appspot.com",
+  messagingSenderId: "377380908366",
+  appId: "1:377380908366:web:66846a742fabd73be8821f",
+  measurementId: "G-R0DLKLZPW5"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
